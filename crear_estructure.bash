0a14bb69fcafdc203a9915bdb706270d24920cae
ealvan <ealvan@unsa.edu.pe>
2022-10-28 22:07:26 -0500

    adding structure like the labortory Tortuise SVN

---
+++ crear_estructure.bash
@@ -0,0 +1,34 @@
+echo "CREANDO ESTRUCTURA DE CARPETA PARA REPOSITORIO DIRIGIDO CON TORTOISE SVN "
+xxx="project_name"
+mkdir -p -v repositorios_svn/proyectos/$xxx/codigo/trunk
+mkdir -p -v repositorios_svn/proyectos/$xxx/codigo/branches
+mkdir -p -v repositorios_svn/proyectos/$xxx/codigo/tags
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk/java
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk/cade
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk/scripts
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk/c
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/trunk/cobol
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/branches
+mkdir -p -v repositorios_svn/proyectos/$xxx/backend/tags
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/estatico/trunk
+mkdir -p -v repositorios_svn/proyectos/$xxx/estatico/branches
+mkdir -p -v repositorios_svn/proyectos/$xxx/estatico/tags
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/config
+mkdir -p -v repositorios_svn/proyectos/$xxx/config/trunk/serapp/xxxConfig/
+mkdir -p -v repositorios_svn/proyectos/$xxx/config/trunk/backend/
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/datos/trunk/
+mkdir -p -v repositorios_svn/proyectos/$xxx/datos/branches/
+mkdir -p -v repositorios_svn/proyectos/$xxx/datos/tags/
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/documentation/trunk
+mkdir -p -v repositorios_svn/proyectos/$xxx/documentation/branches
+mkdir -p -v repositorios_svn/proyectos/$xxx/documentation/tags
+
+mkdir -p -v repositorios_svn/proyectos/$xxx/tags/anthill
+
---
+++ draf.bash
@@ -0,0 +1,3 @@
+echo "HOLA"
+xxx="project_name"
+mkdir -p -v testing_folder/$(xxx)
